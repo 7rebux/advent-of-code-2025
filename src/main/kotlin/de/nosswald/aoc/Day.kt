7ac5785ca33f6281c3ff312d8e78a596c46f199e
Nils Osswald <ossw.nils@gmail.com>
2025-12-01 15:40:14 +0100

    Initial commit

---
+++ src/main/kotlin/de/nosswald/aoc/Day.kt
@@ -0,0 +1,13 @@
+package de.nosswald.aoc
+
+import de.nosswald.aoc.utils.InputReader
+
+abstract class Day<O, T>(val number: Int, val title: String) {
+  private val inputList by lazy { InputReader.readAsList(number) }
+
+  abstract fun partOne(input: List<String> = inputList): O
+  abstract fun partTwo(input: List<String> = inputList): T
+
+  abstract val partOneTestExamples: Map<List<String>, O>
+  abstract val partTwoTestExamples: Map<List<String>, T>
+}
---
+++ src/main/kotlin/de/nosswald/aoc/Main.kt
@@ -0,0 +1,32 @@
+package de.nosswald.aoc
+
+import de.nosswald.aoc.days.*
+
+object Main {
+  private val days = listOf<Day<*, *>>(
+    Day01,
+  )
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    if (args.isNotEmpty()) {
+      val number = args[0].toInt()
+      val day = days.find { it.number == number }
+        ?: error("Day $number not found!")
+
+      println(day)
+    } else {
+      days.forEach(::printDay)
+    }
+  }
+
+  private fun printDay(day: Day<*, *>) {
+    val header = "=== Day ${day.number.toString().padStart(2, '0')}: ${day.title} ==="
+    val footer = "=".repeat(header.length)
+
+    println(header)
+    println("|> Part 1: ${day.partOne()}")
+    println("|> Part 2: ${day.partTwo()}")
+    println(footer)
+  }
+}
